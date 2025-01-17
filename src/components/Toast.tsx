d143fe588288ecfdb554b164e7797f41d42c3133
KoJaem <woalswhwh@naver.com>
2025-01-17 17:04:35 +0900

    chore: 오타수정

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -28,7 +28,7 @@ export default function Toast() {
     <AnimatePresence>
       {toast.isOpened && (
         <motion.section
-          className="fixed bottom-0 top-0 flex w-full justify-center bg-[#171617]/60 px-6 py-10 drop-shadow-lg backdrop-blur-lg"
+          className="fixed bottom-0 flex w-full justify-center bg-[#171617]/60 px-6 py-10 drop-shadow-lg backdrop-blur-lg"
           initial={{ y: 50, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           exit={{ y: 50, opacity: 0 }}
