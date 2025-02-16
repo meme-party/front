87c317f943e609561d9098d0dff963f9ddc35279
KoJaem <woalswhwh@naver.com>
2025-02-16 20:35:53 +0900

    fix: cn 함수 커스텀 클래스 추가

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,6 +1,27 @@
 import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { extendTailwindMerge } from "tailwind-merge"
+
+const customTWMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      "font-size": [
+        "webTitle",
+        "h1-sb",
+        "h1-m",
+        "h2-b",
+        "h2-sb",
+        "h2-m",
+        "h2-r",
+        "h3-sb",
+        "h3-m",
+        "h3-r",
+        "h4-m",
+        "h4-r"
+      ]
+    }
+  }
+})
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return customTWMerge(clsx(inputs))
 }
