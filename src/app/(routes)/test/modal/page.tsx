97b6b0e35dd4a89d37676047003edab8fe5c4d5f
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2025-02-17 13:53:30 +0900

    chore: resolve conflict

--- src/app/(routes)/test/modal/page.tsx
+++ src/app/(routes)/test/modal/page.tsx
@@ -1,31 +1,42 @@
 "use client"
 import { useModalStore } from "@/store/modal"
-import ModalFrame from "@/components/modal/ModalFrame"
+import ConfirmModal from "@/components/modal/ConfirmModal"
+import Button from "@/components/Button"
+import CollectionModal from "@/components/modal/CollectionModal"
 
 export default function Home() {
-  const { openModal } = useModalStore()
+  const { openModal, closeModal } = useModalStore()
 
-  const handleOpenModal = () => {
+  const openLogoutModal = () => {
     openModal(
-      <div>
-        <h2 className="text-xl font-bold">모달 제목</h2>
-        <p className="text-gray-600 mt-2">이것은 재사용 가능한 모달입니다.</p>
-        <button
-          className="mt-4 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
-          onClick={() => alert("버튼 클릭!")}
-        >
-          동작 버튼
-        </button>
-      </div>
+      <ConfirmModal onConfirm={() => {}} onCancel={closeModal}>
+        <p className="text-xl font-medium text-white">로그아웃 하시겠습니까?</p>
+      </ConfirmModal>
     )
+  }
+  const openDeleteAccount = () => {
+    openModal(
+      <ConfirmModal onConfirm={() => {}} onCancel={closeModal}>
+        <p className="text-xl font-medium text-white">탈퇴하면 모든 정보가 사라집니다.</p>
+        <p className="text-xl font-medium text-white">탈퇴하시겠습니까?</p>
+      </ConfirmModal>
+    )
+  }
+  const openCollenctionModal = () => {
+    openModal(<CollectionModal onCancel={closeModal} />)
   }
 
   return (
-    <div className="bg-gray-100 flex min-h-screen items-center justify-center">
-      <button className="rounded bg-blue-500 px-6 py-3 text-white hover:bg-blue-600" onClick={handleOpenModal}>
-        모달 열기
-      </button>
-      <ModalFrame />
+    <div className="flex min-h-screen items-center justify-center gap-10 bg-gray-100">
+      <Button className="text-white" onClick={openLogoutModal}>
+        로그아웃 모달 열기
+      </Button>
+      <Button className="text-white" onClick={openDeleteAccount}>
+        탈퇴 모달 열기
+      </Button>
+      <Button className="text-white" onClick={openCollenctionModal}>
+        컬렉션 모달 열기
+      </Button>
     </div>
   )
 }
--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,28 +1,26 @@
+"use client"
 import React from "react"
 import ReactDOM from "react-dom"
 import { motion } from "framer-motion"
 import { useModalStore } from "@/store/modal"
 
-export default function ModalFrame() {
+export default function Modal() {
   const { isOpen, closeModal, content } = useModalStore()
 
   if (!isOpen) return null
 
   return ReactDOM.createPortal(
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-gray-scale-900 backdrop-blur-2xl"
+      onClick={closeModal}
+    >
       <motion.div
         initial={{ opacity: 0, scale: 0.9 }}
         animate={{ opacity: 1, scale: 1 }}
         exit={{ opacity: 0, scale: 0.9 }}
-        className="relative w-full max-w-lg rounded-lg bg-white p-6 shadow-lg"
+        className="relative rounded-lg bg-gray-scale-800 p-6 shadow-lg"
+        onClick={(e) => e.stopPropagation()}
       >
-        <button
-          className="text-gray-500 hover:text-gray-800 absolute right-4 top-4"
-          onClick={closeModal}
-          aria-label="Close modal"
-        >
-          ✖
-        </button>
         {content}
       </motion.div>
     </div>,
