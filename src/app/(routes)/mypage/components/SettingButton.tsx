ed62c0a52abd5ee9b082bf4611ba343d9b748091
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2025-05-04 16:06:43 +0900

    feat: header button action

--- src/app/(routes)/mypage/components/SettingButton.tsx
+++ src/app/(routes)/mypage/components/SettingButton.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function SettingButton({ text, textColor = "text-gray-scale-100", onClick }: Props) {
   return (
-    <div className="rounded-[8px] bg-gray-scale-800 px-[24px] py-[12px]" onClick={onClick}>
+    <div className="cursor-pointer rounded-[8px] bg-gray-scale-800 px-[24px] py-[12px]" onClick={onClick}>
       <p className={textColor}>{text}</p>
     </div>
   )
