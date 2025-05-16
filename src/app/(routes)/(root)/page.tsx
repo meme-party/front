b95b72b7064750639d1a698769b57584e49975ed
KoJaem <woalswhwh@naver.com>
2025-05-16 15:34:15 +0900

    feat: 메인페이지 슬라이드 UI추가

--- src/app/(routes)/(root)/page.tsx
+++ src/app/(routes)/(root)/page.tsx
@@ -6,6 +6,12 @@ import TagButton from "@/components/TagButton"
 import { COLORS } from "@/styles/colors"
 import { Type } from "lucide-react"
 import { useRouter } from "next/navigation"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Autoplay, Pagination } from "swiper/modules"
+import "swiper/css"
+import "swiper/css/pagination"
+import "@/styles/sliderBullet.css"
+import Image from "next/image"
 
 export default function Home() {
   const router = useRouter()
@@ -14,6 +20,33 @@ export default function Home() {
       <section className="flex justify-center lg:justify-between">
         <Sidebar />
         <section className="flex w-full max-w-[712px] flex-col gap-[32px] lg:mt-[64px]">
+          <section className="mx-[-16px] md:mx-[-28px] lg:mx-0">
+            <Swiper
+              className="relative h-[210px] w-full md:h-[320px] lg:rounded-[16px]"
+              modules={[Pagination, Autoplay]}
+              pagination={{
+                el: `#root-slider`,
+                clickable: true,
+                renderBullet: function (_, className) {
+                  return `<span class="${className} !opacity-100 !bg-gray-scale-600 !w-[8px] !h-[8px]"> </span>`
+                }
+              }}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false
+              }}
+            >
+              {Array.from({ length: 2 }).map((_, index) => (
+                <SwiperSlide key={index} className="!w-full">
+                  <Image src={`/root/slide${index + 1}.png`} alt="slide" fill className="object-cover" />
+                </SwiperSlide>
+              ))}
+              <section
+                id="root-slider"
+                className="absolute bottom-[8px] z-[10] flex h-[12px] w-full justify-center gap-[4px]"
+              />
+            </Swiper>
+          </section>
           <section className="flex flex-col gap-[16px]">
             <div className="flex flex-col items-start gap-[8px] md:flex-row md:items-center md:gap-[16px]">
               <p className="text-h2-sb text-gray-scale-100 md:text-h1-sb">유형별 밈</p>
